e506aefe5003aa23a8a2879429b360b2873d3a93
Shengbo Xu <to.be.mr.all.rounder@gmail.com>
2015-03-29 20:20:17 +0900

    first commit

---
+++ Berksfile
@@ -0,0 +1,3 @@
+site :opscode
+
+cookbook 'rbenv', git: 'https://github.com/fnichol/chef-rbenv'
---
+++ site-cookbooks/utils/recipes/default.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# TODO cookbook分割した方がいいと思うんだけど、
+# 手元の開発環境整備だけだし、とりあえずこれで
+
+
+gem_package "rbenv-rehash" do
+  action :install
+end
+
+execute "resolv.option" do
+  command "echo 'options single-request-reopen' >> /etc/resolv.conf"
+  action :run
+end
+
+execute "gem.update" do
+  command "gem update --system"
+  action :run
+end
+
+execute "rails.install" do
+  command "gem install --no-ri --no-rdoc rails"
+  action :run
+end
+
+gem_package "bundler" do
+  action :install
+end
+
+%w{gcc make wget emacs}.each do |p|
+  package p do
+    action :install
+  end
+end
+
+%w{git libxml2}.each do |p|
+  package p do
+    action :install
+  end
+end
